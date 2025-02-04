7e64cf0b77d1d56b7f65d8a7122459cca1454060
khaquachtrong74 <khaquachtrong74@gmail.com>
2025-02-04 10:21:32 +0700

    Fix input

--- Cpp/Algorithm/main.cpp
+++ Cpp/Algorithm/main.cpp
@@ -3,8 +3,9 @@ int main(){
 	string input;
 	cout<<"Nhap so luong phan tu: ";cin>>input;
 	ARR al(stoi(input));
+	cin.ignore();
 	cout<<"Nhap cac phan tu duoc ngan boi bat ky dau: ";
-	cin>>input;
+	getline(cin, input);
 	al.input(input);
 	al.output();
 }
